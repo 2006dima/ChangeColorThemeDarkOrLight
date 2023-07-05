ed536c69ba8da53438e6723ac213574b02b525f4
2006dima <137100732+2006dima@users.noreply.github.com>
2023-07-05 13:52:53 +0400

    Changed photos

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 
 
-let getP = document.getElementById("id");
+let image1 = document.getElementById("moon-1")
+let image2 = document.getElementById("moon-2");
 let button = document.getElementById("button");
+let marquee = document.getElementById("marquee");
+
 document.getElementById("button").addEventListener("click", myFunction());
 function myFunction(){
     let  element = document.body;
@@ -10,10 +13,19 @@ function myFunction(){
         element.classList.toggle("dark")
         button.textContent = "light";    
         id.style.color = "white"; 
+        id.innerText = "Dark Theme";
+        image2.style.display = "none";
+        image1.style.display = "block";
+
     } else {
         element.classList.toggle("light")
         button.textContent = "dark"
         id.style.color = "black";
+        id.innerText = "Light Theme";
+        image2.style.display = "block";
+        image1.style.display = "none";
+
+
     }
 }
 myFunction()
